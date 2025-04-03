acb4f1c6a9fd11857d1faf222408cb19b98cee19
nonnymox <knonxho@gmail.com>
2025-04-03 04:32:17 -0700

    Initialized app and added dependencies

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+// CommonJS method to import express
+// const express = require('express')
+
+// ES6
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server started at http://localhost:3000");
+});
